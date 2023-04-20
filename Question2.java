15c734cfce1314b21ddfbbd2734a7d52cbfd5e55
H-Joe-Kit <115964672+H-Joe-Kit@users.noreply.github.com>
2023-04-20 11:29:49 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -5,15 +5,15 @@ public class Question2 {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		// height prompt
-		System.out.println("Enter your height in meters: ");
+		// System.out.println("Enter your height in meters: ");
 		double height = in.nextDouble();
 
 		// weight prompt
-		System.out.println("Enter your weight in kilograms: ");
+		// System.out.println("Enter your weight in kilograms: ");
 		double weight = in.nextDouble();
 
 		// BMI calculation & ouput
-    	double bmi = weight / (height * height);
-    	System.out.println("Your BMI is " + bmi);
+    		double bmi = weight / (height * height);
+    		System.out.println(bmi);
 	}
 }
